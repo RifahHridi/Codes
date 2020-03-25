d7531c5770a9da05741fe3150f7253b9d9c33054
RifahHridi <62611549+RifahHridi@users.noreply.github.com>
2020-03-25 16:53:25 +0800

    random code practice

---
+++ JavaApplication10.java
@@ -0,0 +1,25 @@
+
+package javaapplication10;
+
+
+public class JavaApplication10 {
+    
+    
+    public static void main(String[] args) {
+        forobject getset = new forobject (16);
+        forobject getset1 = new forobject (16,15);
+        forobject getset2 = new forobject (16,15,20);
+       
+         System.out.printf("%s\n", getset.toMilitary());
+          System.out.printf("%s\n", getset1.toMilitary());
+           System.out.printf("%s\n", getset2.toMilitary());
+    
+           
+           staticVar member1 =new staticVar("megan","fox");
+           staticVar member2 =new staticVar("Taylor","swift");
+           
+    }
+    
+    
+    
+}
