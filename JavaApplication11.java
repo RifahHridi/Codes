96d78369148ab0a99990b673d0e62b12b64f44c6
RifahHridi <62611549+RifahHridi@users.noreply.github.com>
2020-03-25 16:53:58 +0800

    random code practice 2

---
+++ JavaApplication11.java
@@ -0,0 +1,13 @@
+
+package javaapplication11;
+
+
+public class JavaApplication11 {
+
+    
+    public static void main(String[] args) {
+        setting data =new setting();
+        System.out.printf("%d %d %d ",getName(),getPopulation(),getgrowthRate());
+    }
+    
+}
