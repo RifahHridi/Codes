f7dc3fe53c65626255b1af1a95e6eae2df97d2de
RifahHridi <62611549+RifahHridi@users.noreply.github.com>
2020-03-25 16:54:45 +0800

    random code practice java 3

---
+++ prac2.java
@@ -0,0 +1,28 @@
+
+package javaapplication14;
+
+
+public class prac2 {
+    
+    public static String first="FARHAD";
+    public  static String last="HRIDI";
+    public static String first1= "M";
+    public static int members=0;
+    
+
+            
+       
+            
+public static void main(String []args){
+    
+    for (int i=1;i<=100;i++){
+        System.out.println(first);
+        System.out.println(last);
+        
+        }
+  
+ 
+members++;
+System.out.println(members);
+}
+}
