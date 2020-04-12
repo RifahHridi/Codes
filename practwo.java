f2241cfd96b091cf8a6273c00ee2979273ffd40a
RifahHridi <62611549+RifahHridi@users.noreply.github.com>
2020-04-12 13:57:42 +0600

    swap two numbers basic

--- practwo.java
+++ practwo.java
@@ -2,22 +2,13 @@
 public class practwo {
     //i want to swap two rows the first and the last row
    public static void main (String[] args){
-    int m[][]={{1,2,3,4},
-              {1,2,3,4},
-              {1,2,3,4},
-              {1,2,3,4}};
-    int i = 0;
-    int temp= m[0][i];
-    m[0][i]= m[m.length-1][i];
-    m[m.length-1][i]= temp;
+   int i=1,j=2;
+    int temp= i;
+    i=j;
+    j= temp;
     
-    for ( i=0;i<m.length;i++){
-        for(int j=0;j<m[0].length;j++){
-            System.out.println(+i+j);
-        }
-    }
-     }
+    System.out.println(""+i);
        
-
+   }
 }
 
